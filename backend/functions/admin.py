018ce66de9612d87771c9f4ea3d25bda6faecdfd
Vitaly <mr.peunov@gmail.com>
2021-08-16 12:45:36 +0300

    Prototype of main function

--- backend/functions/admin.py
+++ backend/functions/admin.py
@@ -1,20 +1,31 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
+
 from .models import Function
+from .services import create_graph
 
 
 @admin.register(Function)
 class FunctionAdmin(admin.ModelAdmin):
-    list_display = ['function', 'graph', 'interval', 'dt', 'update_date']
-    readonly_fields = ['graph']
+    list_display = ['string_view', 'get_graph', 'interval', 'dt', 'update_date']
+    readonly_fields = ['graph', 'status']
     actions = ['update_graph']
 
     @admin.action(description='Обновить')
     def update_graph(self, request, queryset):
         for function_item in queryset:
-            function_item.create_graph()
+            create_graph(function_item.pk)
 
     def get_actions(self, request):
         actions = super().get_actions(request)
         if 'delete_selected' in actions:
             del actions['delete_selected']
         return actions
+
+    def get_graph(self, obj):
+        result = '<p>{}</p>'.format(obj.status)
+
+        if obj.graph:
+            result = '<img src={} width="100" height="100">'.format(obj.graph.url)
+
+        return mark_safe(result)
--- backend/functions/models.py
+++ backend/functions/models.py
@@ -2,15 +2,18 @@ from django.db import models
 
 
 class Function(models.Model):
-    function = models.CharField('Функция', max_length=128)
+    string_view = models.CharField('Функция', max_length=128)
     graph = models.ImageField('График', blank=True)
     interval = models.IntegerField('Интервал t, дней')
     dt = models.IntegerField('Шаг t, часы')
+    status = models.CharField('Статус', max_length=64, default='Нет изображения')
     update_date = models.DateTimeField('Дата  обработки', auto_now=True)
 
     def save(self, *args, **kwargs):
-        self.create_graph()
         super().save(*args, **kwargs)
+        print("Создание графа", self.string_view)
+        # create_graph(self.pk)
 
-    def create_graph(self):
-        print("Создание графа", self.function)
+    # def create_graph(self):
+    #     # print("Создание графа", self.string_view)
+
